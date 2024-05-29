910590bf4fb6b1ab62ccf9cd894432d195ec9a72
ALEX Wilson <530515490@qq.com>
2024-05-29 23:34:14 +0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Component as CreditCardGenerator } from  '../components/component/component';
+export default function Home() {
+  return (
+    <div>
+      
+      <CreditCardGenerator />
+    </div>
+  );
+}
